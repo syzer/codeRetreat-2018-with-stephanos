3844ad6134c2e43c7a4ff223ef5f481ee3016991
Kidlike <steve.tcmg@gmail.com>
2018-11-17 12:46:34 +0100

    add isEmpty

--- test/Grid.js
+++ test/Grid.js
@@ -3,9 +3,11 @@ import Point from '../src/Point'
 import Cell from '../src/Cell'
 import Grid from '../src/Grid'
 
+const isEmpty = (arr) => arr.length === 0
+
 test('grid_can_add_cells', t => {
     const cell = new Cell(new Point(1, 2))
     const grid = new Grid()
     grid.addCell(cell)
-    t.truthy(grid.cells.length >= 1, "grid.cells")
+    t.falsy(isEmpty(grid.cells), "grid.cells")
 })
